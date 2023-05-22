b117b6e8602f9f3ac994e1f9894c0b8a269d45e5
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2023-05-22 08:50:37 -0400

    send without template files to test

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,15 @@ const app: Express = express();
 app.use(bodyParser.json());
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+  const today = new Date().getDay();
+  if (today === 6 || today === 0) {
+    res.write("<p>Yay it's the weekend!</p>");
+    res.write('<h1>I can do what I want</h1>');
+  } else {
+    // res.write('<h1>It is not the weekend</h1>');
+    // res.write('<p>Boo! I have to work!</p>');
+    res.sendFile(__dirname + '/index.html');
+  }
 });
 
 app.listen(3000, () => {
