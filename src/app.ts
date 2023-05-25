00c569cb8f7ae9a761697a67afddba41c8cf13eb
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2023-05-25 08:24:22 -0400

    add templates for work and about pages with styling

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,53 @@
 import express, { Express, Request, Response } from 'express';
 import bodyParser from 'body-parser';
+import path from 'path';
 
 const app: Express = express();
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../', 'public')));
+
+const items: string[] = ['Buy Food', 'Cook Food', 'Eat Food'];
+const workItems: string[] = [];
 
 app.get('/', (req: Request, res: Response) => {
-  const today = new Date().getDay();
-  if (today === 6 || today === 0) {
-    res.write("<p>Yay it's the weekend!</p>");
-    res.write('<h1>I can do what I want</h1>');
+  const today = new Date();
+  const options: Intl.DateTimeFormatOptions = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+  };
+  const day = today.toLocaleDateString('en-US', options);
+
+  res.render('list', { listTitle: day, items });
+});
+
+app.post('/', (req: Request<{ newItem: string }>, res: Response) => {
+  const { newItem } = req.body;
+  if (req.body.list === 'Work') {
+    workItems.push(newItem);
+    res.redirect('/work');
   } else {
-    // res.write('<h1>It is not the weekend</h1>');
-    // res.write('<p>Boo! I have to work!</p>');
-    res.sendFile(__dirname + '/index.html');
+    items.push(newItem);
+    res.redirect('/');
   }
+});
+
+app.get('/work', (req: Request, res: Response) => {
+  res.render('list', { listTitle: 'Work List', items: workItems });
+});
+
+app.post('/work', (req: Request<{ newItem: string }>, res: Response) => {
+  const { newItem } = req.body;
+  workItems.push(newItem);
+  res.redirect('/work');
+});
+
+app.get('/about', (req: Request, res: Response) => {
+  res.render('about');
 });
 
 app.listen(3000, () => {
